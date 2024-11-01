f7b306d94662466dae03a10f35688a5dbfb7b3b5
rejhr <hyoreong@reconers.com>
2024-11-01 18:30:49 +0900

    modified:   three.js 	new file:   threejs/reconers_v2.glb

--- three.js
+++ three.js
@@ -116,7 +116,7 @@ const cubeMap = new THREE.CubeTextureLoader().load([
 
 // ============ Meshes ============
 // GLTF Mesh
-new GLTFLoader().load("./threejs/reconers.glb", (gltf) => {
+new GLTFLoader().load("./threejs/reconers_v2.glb", (gltf) => {
   const model = gltf.scene; // 3D 파일에서 Scene 전체 로드
 
   // Mesh 정의
