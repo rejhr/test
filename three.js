319d5dff1391e31f8838a7941ede077481ba7b73
rejhr <hyoreong@reconers.com>
2024-11-04 10:35:07 +0900

    modified:   three.js

--- three.js
+++ three.js
@@ -98,7 +98,7 @@ new GLTFLoader().load("./threejs/reconers_v9.glb", (gltf) => {
           side: THREE.FrontSide,
           blending: THREE.NormalBlending,
           // blending: THREE.AdditiveBlending,
-          // opacity: 0.6, // 불투명도
+          opacity: 0.8, // 불투명도
           color: 0x0B6FE8, // 색상
           // transmission: 1, // 투과성
           reflectivity: 0.5, // 반사도
