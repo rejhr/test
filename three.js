6b1f87e7cbd9a151b78ed4e62c8195b97709f2a4
rejhr <hyoreong@reconers.com>
2024-11-05 19:30:58 +0900

    modified:   three.js

--- three.js
+++ three.js
@@ -6,6 +6,7 @@ import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 import { RGBELoader } from "three/addons/loaders/RGBELoader.js";
 import { EffectComposer } from "three/addons/postprocessing/EffectComposer.js";
 import { RenderPass } from "three/addons/postprocessing/RenderPass.js";
+import { ShaderPass } from "three/addons/postprocessing/ShaderPass.js";
 import { UnrealBloomPass } from "three/addons/postprocessing/UnrealBloomPass.js";
 import { LuminosityHighPassShader } from "three/addons/shaders/LuminosityHighPassShader.js";
 import { CopyShader } from "three/addons/shaders/CopyShader.js";
