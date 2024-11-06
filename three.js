f9eac609236f31b0021aca6e258427c7307d29d9
rejhr <hyoreong@reconers.com>
2024-11-06 13:07:42 +0900

    modified:   three.js

--- three.js
+++ three.js
@@ -176,14 +176,18 @@ new GLTFLoader().load("./threejs/reconers_v30.glb", (gltf) => {
   console.log(BLOOM_SCENE)
 
   // ============ 렌더 합성 ============
+  const clearPass = new ClearPass();  // 프레임마다 클리어를 추가
+
   // 기본 장면과 bloom 장면을 분리해 렌더링하도록 설정
   const bloomComposer = new EffectComposer(renderer);
+  bloomComposer.addPass(clearPass); // 클리어 패스 추가
   bloomComposer.addPass(renderPass);
   bloomComposer.addPass(bloomPass);
   bloomComposer.renderToScreen = false; // 최종 화면에 직접 출력하지 않음
   
   // 씬 마스크 설정
   const darkComposer = new EffectComposer(renderer);
+  darkComposer.addPass(clearPass); // 클리어 패스 추가
   darkComposer.addPass(renderPass);
   darkComposer.renderToScreen = false; // 최종 화면에 직접 출력하지 않음
   
@@ -202,7 +206,9 @@ new GLTFLoader().load("./threejs/reconers_v30.glb", (gltf) => {
   finalPass.needsSwap = true;
   
   const finalComposer = new EffectComposer(renderer); 
+  finalComposer.addPass(clearPass); // 최종 렌더링 시에도 클리어 추가
   finalComposer.addPass(finalPass);
+  finalComposer.renderToScreen = true;  // 최종 컴포저에서만 화면에 렌더링
 
 // ============ 애니메이션 ============
 
