13b7d9dd9e2bcc951184c350a95fd0e65c217768
rejhr <hyoreong@reconers.com>
2024-11-07 14:20:47 +0900

    modified:   three.js

--- three.js
+++ three.js
@@ -127,7 +127,7 @@ new GLTFLoader().load("./threejs/reconers_v31.glb", (gltf) => {
         const normalMesh = new THREE.Mesh(geometry, materialNormal);
         const AdditivetMesh = new THREE.Mesh(geometry, materialAdditive);
 
-        materialAdditive.scale.set(0.1, 0.22, 0.1);
+        AdditivetMesh.scale.set(0.1, 0.22, 0.1);
         normalMesh.scale.set(0.1, 0.22, 0.1);
         
         reconers.add(normalMesh);
