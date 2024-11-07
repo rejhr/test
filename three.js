03f62a3f05b45fc09c6d48cdb24c3d4a253d4a22
rejhr <hyoreong@reconers.com>
2024-11-07 19:35:06 +0900

    modified:   three.js

--- three.js
+++ three.js
@@ -107,7 +107,7 @@ new GLTFLoader().load("./threejs/reconers.glb", (gltf) => {
         // 뒷면 내부 입체감
         const materialNormal = new THREE.MeshPhysicalMaterial({
           blending: THREE.NormalBlending,
-          color: 0x100D59, // 색상
+          color: 0x0B6FE8, // 색상
           transmission: 1, // 투과성
           reflectivity: 1, // 반사
           metalness: 1, // 금속질
