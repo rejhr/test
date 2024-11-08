b7d34868400b26342cd6e1c7d5536b81368ba290
rejhr <hyoreong@reconers.com>
2024-11-08 11:54:03 +0900

    modified:   three.js

--- three.js
+++ three.js
@@ -57,6 +57,8 @@ const hdrEquirect = new RGBELoader().load(
   }
 );
 
+scene.envMap = hdrEquirect;
+
 const cubeMap = new THREE.CubeTextureLoader().load([
     './threejs/cubeMap_01.png', // 오른쪽(px)
     './threejs/cubeMap_02.png', // 왼쪽(nx)
@@ -98,7 +100,7 @@ new GLTFLoader().load("./threejs/reconers_v2.glb", (gltf) => {
           sheen: 1, // 미광 광택 적용값
           sheenRoughness: 0.2, // 미광 표면 거칠기
           sheenColor: 0x0B6FE8, // 미광 색상
-          envMap: hdrEquirect,  // 환경맵
+          envMap: cubeMap,  // 환경맵
           envMapIntensity: 0.5, // 환경맵 적용값
           // alphaToCoverage: true,
         }); 
