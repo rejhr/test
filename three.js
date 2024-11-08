3e7ee2f3e85c0e71b177eb5a55f631a32f2f332e
rejhr <hyoreong@reconers.com>
2024-11-08 12:11:57 +0900

    modified:   three.js

--- three.js
+++ three.js
@@ -104,7 +104,7 @@ new GLTFLoader().load("./threejs/reconers_v2.glb", (gltf) => {
           sheen: 1, // 미광 광택 적용값
           sheenRoughness: 0.2, // 미광 표면 거칠기
           sheenColor: 0x0B6FE8, // 미광 색상
-          envMap: hdrEquirect,  // 환경맵
+          envMap: cubeMap,  // 환경맵
           envMapIntensity: 0.5, // 환경맵 적용값
           // alphaToCoverage: true,
         }); 
