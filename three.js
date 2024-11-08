5444bcfa58a12b57c739e439654058bb23bc0bde
rejhr <hyoreong@reconers.com>
2024-11-08 11:52:55 +0900

    modified:   three.js

--- three.js
+++ three.js
@@ -86,8 +86,8 @@ new GLTFLoader().load("./threejs/reconers_v2.glb", (gltf) => {
           color: 0xDCDDE4, // 색상
           // color: 0x100D59, // 색상
           transmission: 1, // 투과성
-          reflectivity: 0.8, // 반사
-          metalness: 0.1, // 금속질
+          reflectivity: 0.5, // 반사
+          metalness: 0.05, // 금속질
           roughness: 0.05, // 표면 거칠기
           ior: 2, // 굴절률
           iridescence: 1, // 표면 RGB 왜곡
@@ -99,7 +99,7 @@ new GLTFLoader().load("./threejs/reconers_v2.glb", (gltf) => {
           sheenRoughness: 0.2, // 미광 표면 거칠기
           sheenColor: 0x0B6FE8, // 미광 색상
           envMap: hdrEquirect,  // 환경맵
-          envMapIntensity: 1, // 환경맵 적용값
+          envMapIntensity: 0.5, // 환경맵 적용값
           // alphaToCoverage: true,
         }); 
 
