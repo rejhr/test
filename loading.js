188b40531166be5441f4d0e29dcaddd39c5f3cd0
rejhr <hyoreong@reconers.com>
2024-11-11 17:28:56 +0900

    modified:   loading.js

--- loading.js
+++ loading.js
@@ -1,37 +1,37 @@
 document.body.style.overflow = 'hidden';
 
+const intro = this.document.querySelector('#intro');
+
 setTimeout(() => {
-  const intro = this.document.querySelector('#intro');
-}, 3000);
-
 window.onload = function () {
-  intro.classList.add('fadeOut', 'trasition');
-  document.body.style.overflow = 'unset';
+    intro.classList.add('fadeOut', 'trasition');
+    document.body.style.overflow = 'unset';
+    
+    if(intro.style.display = 'none'){
+      const GNB = document.querySelector('.GNB');
+      GNB.classList.add('fadeIn', 'trasition');
+      
+      const fotter = document.querySelector('footer');
+      fotter.classList.add('fadeIn', 'trasition');
+      
+      const three = document.querySelector('#three');
+      three.classList.add('scaleUp');
+      
+      const bgText = document.querySelector('.bgText');
+      const bgTextP = bgText.querySelectorAll('.word');
+      bgTextP.forEach(function(p) {
+        p.classList.add('slideUp', 'trasition', 'durationXL');
+      });
+      
+      const displayText = document.querySelector('.displayText');
+      displayText.classList.add('slideRignt', 'trasition', 'delayXL');
+      
+      const quickMenu = document.querySelector('.quickMenu');
+      quickMenu.classList.add('slideLeft','trasition','delayXL');
+    }
+  };
+}, 3000);
   
-  if(intro.style.display = 'none'){
-    const GNB = document.querySelector('.GNB');
-    GNB.classList.add('fadeIn', 'trasition');
-    
-    const fotter = document.querySelector('footer');
-    fotter.classList.add('fadeIn', 'trasition');
-    
-    const three = document.querySelector('#three');
-    three.classList.add('scaleUp');
-    
-    const bgText = document.querySelector('.bgText');
-    const bgTextP = bgText.querySelectorAll('.word');
-    bgTextP.forEach(function(p) {
-      p.classList.add('slideUp', 'trasition', 'durationXL');
-    });
-    
-    const displayText = document.querySelector('.displayText');
-    displayText.classList.add('slideRignt', 'trasition', 'delayXL');
-    
-    const quickMenu = document.querySelector('.quickMenu');
-    quickMenu.classList.add('slideLeft','trasition','delayXL');
-  }
-};
-
-
-
+  
+  
  
