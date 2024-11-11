4b7e176c889ada9f653ac73b05f2a34930df3541
rejhr <hyoreong@reconers.com>
2024-11-11 14:48:06 +0900

    modified:   loading.js

--- loading.js
+++ loading.js
@@ -6,24 +6,24 @@ window.onload = function () {
   intro.classList.add('fadeOut', 'trasition', 'durationL');
   document.body.style.overflow = 'unset';
 
-  const GNB = document.querySelector('GNB');
+  const GNB = document.querySelector('.GNB');
   GNB.classList.add('fadeIn','trasition');
 
-  const fotter = document.querySelector('footer');
+  const fotter = document.querySelector('.footer');
   GNB.footer.add('fadeIn','trasition');
 
   const three = document.querySelector('#three');
   three.classList.add('scaleUp');
 
-  const bgText = document.getElementsByClassName('bgText');
+  const bgText = document.getElementsByClassName('.bgText');
   const bgTextP = bgText.querySelectorAll('p');
   bgTextP.forEach(function(p) {
     p.classList.add('slideUp', 'trasition', 'durationXL');
   });
   
-  const displayText = document.querySelector('displayText');
+  const displayText = document.querySelector('.displayText');
   displayText.classList.add('slideRignt', 'trasition', 'delayXL');
 
-  const quickMenu = document.querySelector('quickMenu');
+  const quickMenu = document.querySelector('.quickMenu');
   quickMenu.classList.add('slideLeft','trasition','delayXL');
 };
