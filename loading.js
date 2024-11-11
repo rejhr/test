8d13737a8123b02f78dea74247b53983dd1d6d13
rejhr <hyoreong@reconers.com>
2024-11-11 15:06:10 +0900

    modified:   loading.js

--- loading.js
+++ loading.js
@@ -8,24 +8,24 @@ window.onload = function () {
 
   if(intro.style.display = 'none'){
     const GNB = document.querySelector('.GNB');
-  GNB.classList.add('fadeIn', 'trasition');
-
-  const fotter = document.querySelector('.footer');
-  fotter.classList.add('fadeIn', 'trasition');
-
-  const three = document.querySelector('#three');
-  three.classList.add('scaleUp');
-
-  const bgText = document.getElementsByClassName('.bgText');
-  const bgTextP = bgText.querySelectorAll('p');
-  bgTextP.forEach(function(p) {
-    p.classList.add('slideUp', 'trasition', 'durationXL');
-  });
+    GNB.classList.add('fadeIn', 'trasition');
   
-  const displayText = document.querySelector('.displayText');
-  displayText.classList.add('slideRignt', 'trasition', 'delayXL');
-
-  const quickMenu = document.querySelector('.quickMenu');
-  quickMenu.classList.add('slideLeft','trasition','delayXL');
-  };
+    const fotter = document.querySelector('.footer');
+    fotter.classList.add('fadeIn', 'trasition');
+  
+    const three = document.querySelector('#three');
+    three.classList.add('scaleUp');
+  
+    const bgText = document.getElementsByClassName('.bgText');
+    const bgTextP = bgText.querySelectorAll('p');
+    bgTextP.forEach(function(p) {
+      p.classList.add('slideUp', 'trasition', 'durationXL');
+    });
+    
+    const displayText = document.querySelector('.displayText');
+    displayText.classList.add('slideRignt', 'trasition', 'delayXL');
+  
+    const quickMenu = document.querySelector('.quickMenu');
+    quickMenu.classList.add('slideLeft','trasition','delayXL');
+  }
 };
