3e3ffe9f003331852a33cce55479d1f5a2f708d6
rejhr <hyoreong@reconers.com>
2024-11-11 10:22:31 +0900

    modified:   three.js

--- three.js
+++ three.js
@@ -89,7 +89,7 @@ new GLTFLoader().load("./threejs/reconers_v2.glb", (gltf) => {
           transmission: 1, // 투과성
           reflectivity: 0.8, // 반사
           metalness: 0.1, // 금속질
-          roughness: 0.2, // 표면 거칠기
+          roughness: 0.1, // 표면 거칠기
           ior: 2, // 굴절률
           iridescence: 1, // 표면 RGB 왜곡
           // clearcoat: 1, // 매끈한 광택 표면 두께감
@@ -112,7 +112,7 @@ new GLTFLoader().load("./threejs/reconers_v2.glb", (gltf) => {
           // transmission: 1, // 투과성
           reflectivity: 1, // 반사
           metalness: 0.4, // 금속질
-          roughness: 0.05, // 표면 거칠기
+          roughness: 0.1, // 표면 거칠기
           ior: 1.5, // 굴절률
           iridescence: 1, // 표면 RGB 왜곡
           // clearcoat: 1, // 매끈한 광택 표면 두께감
