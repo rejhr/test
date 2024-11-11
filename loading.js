1ed0cbf5067c369356e70a2d1fd90642b0ba6605
rejhr <hyoreong@reconers.com>
2024-11-11 14:44:02 +0900

    modified:   index.html 	modified:   loading.js

--- loading.js
+++ loading.js
@@ -1,11 +1,29 @@
 document.body.style.overflow = 'hidden';
 
-let intro = this.document.querySelector('#intro');
+const intro = this.document.querySelector('#intro');
 
 window.onload = function () {
   intro.classList.add('fadeOut', 'trasition', 'durationL');
   document.body.style.overflow = 'unset';
-  
-  let three = document.querySelector('#three');
+
+  const GNB = document.querySelector('GNB');
+  GNB.classList.add('fadeIn','trasition');
+
+  const fotter = document.querySelector('footer');
+  GNB.footer.add('fadeIn','trasition');
+
+  const three = document.querySelector('#three');
   three.classList.add('scaleUp');
+
+  const bgText = document.getElementsByClassName('bgText');
+  const bgTextP = bgText.querySelectorAll('p');
+  bgTextP.forEach(function(p) {
+    p.classList.add('slideUp', 'trasition', 'durationXL');
+  });
+  
+  const displayText = document.querySelector('displayText');
+  displayText.classList.add('slideRignt', 'trasition', 'delayXL');
+
+  const quickMenu = document.querySelector('quickMenu');
+  quickMenu.classList.add('slideLeft','trasition','delayXL');
 };
