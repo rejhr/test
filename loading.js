f150e8eb9c6101779d28bda068df6f348d1b46ec
rejhr <hyoreong@reconers.com>
2024-11-12 11:55:40 +0900

    modified:   loading.js

--- loading.js
+++ loading.js
@@ -7,34 +7,34 @@ window.onload = function () {
       document.body.style.overflow = 'unset';
 
       const introOpacity = getComputedStyle(intro).opacity;
-
-      setTimeout(() => {
-        intro.addEventListener('transitionend', function () {
-          intro.style.display = 'none';
-    
-          const GNB = document.querySelector('.GNB');
-          GNB.classList.add('fadeIn', 'trasition');
-          
-          const footer = document.querySelector('footer');
-          footer.classList.add('fadeIn', 'trasition');
-          
-          const three = document.querySelector('#three');
-          three.classList.add('scaleUp');
-          
-          const bgText = document.querySelector('.bgText');
-          const bgTextP = bgText.querySelectorAll('.word');
-          bgTextP.forEach(function(p) {
-            p.classList.add('slideUp', 'trasition', 'durationXL');
-          });
-          
-          const displayText = document.querySelector('.displayText');
-          displayText.classList.add('slideRight', 'trasition', 'delayXL');
-          
-          const quickMenu = document.querySelector('.quickMenu');
-          quickMenu.classList.add('slideLeft','trasition','delayXL');
-        }, { once: true });
-      }, 3000);
   };
+
+  setTimeout(() => {
+    intro.addEventListener('transitionend', function () {
+      intro.style.display = 'none';
+
+      const GNB = document.querySelector('.GNB');
+      GNB.classList.add('fadeIn', 'trasition');
+      
+      const footer = document.querySelector('footer');
+      footer.classList.add('fadeIn', 'trasition');
+      
+      const three = document.querySelector('#three');
+      three.classList.add('scaleUp');
+      
+      const bgText = document.querySelector('.bgText');
+      const bgTextP = bgText.querySelectorAll('.word');
+      bgTextP.forEach(function(p) {
+        p.classList.add('slideUp', 'trasition', 'durationXL');
+      });
+      
+      const displayText = document.querySelector('.displayText');
+      displayText.classList.add('slideRight', 'trasition', 'delayXL');
+      
+      const quickMenu = document.querySelector('.quickMenu');
+      quickMenu.classList.add('slideLeft','trasition','delayXL');
+    }, { once: true });
+  }, 3000);
   
   
  
